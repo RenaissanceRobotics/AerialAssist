8f91242ba3c872f8bb4f52117245dee4a887ba14
Renaissance Robotics #2 <rambot4525@gmail.com>
2014-04-02 10:06:21 -0400

    Reverted the Testing in Netbeans 1

--- src/edu/wpi/first/wpilibj/templates/Robot.java
+++ src/edu/wpi/first/wpilibj/templates/Robot.java
@@ -13,8 +13,6 @@ import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-// Just Testing making a change to work with git...
-
 public class Robot extends SimpleRobot {
 
     private XboxController xbox1, xbox2;
