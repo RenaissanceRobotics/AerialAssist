0a9d7415d2346901ba6b16f20f83089dffba206c
Renaissance Robotics #2 <rambot4525@gmail.com>
2014-04-03 12:12:34 -0400

    Reveresed the arms and grip closing

--- src/edu/wpi/first/wpilibj/templates/Robot.java
+++ src/edu/wpi/first/wpilibj/templates/Robot.java
@@ -135,14 +135,14 @@ public class Robot extends SimpleRobot {
             // Driver Two
             // ------------------- Arm Control ---------------------
             if (this.xbox2.getY(GenericHID.Hand.kLeft) >= 0.5) { // Joystick forward
+                // Lower Arms
+                this.arms.extend();
+            } else if (this.xbox2.getY(GenericHID.Hand.kLeft) <= -0.5) { // Joystick backward
                 // Raise Arms
-                if (this.grip.isExtended()) { // Can't Raise Arms With Grip Open
+                 if (this.grip.isExtended()) { // Can't Raise Arms With Grip Open
                     this.grip.retract();
                     Timer.delay(0.25);
                 }
-                this.arms.extend();
-            } else if (this.xbox2.getY(GenericHID.Hand.kLeft) <= -0.5) { // Joystick backward
-                // Lower Arms
                 this.arms.retract();
             }
 
